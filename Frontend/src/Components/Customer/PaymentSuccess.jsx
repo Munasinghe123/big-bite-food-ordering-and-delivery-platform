992d5da82f7988666ee9787a44fc01c799e15d5d
sdinukarathnayake <it22140616@my.sliit.lk>
2025-04-29 12:44:12 +0530

    Updated payment success logic

--- Frontend/src/Components/Customer/PaymentSuccess.jsx
+++ Frontend/src/Components/Customer/PaymentSuccess.jsx
@@ -19,8 +19,14 @@ function PaymentSuccess() {
       }
 
       try {
-        // Step 1: Update payment status to "Paid"
-        await axios.put(`http://localhost:5000/orders/mark-paid/${orderId}`,{withCredentials: true});
+        await axios.put(
+          `http://localhost:5000/orders/mark-paid/${orderId}`,
+          {}, 
+          {
+            withCredentials: true
+          }
+        );
+        
         console.log('Payment status updated successfully!');
         toast.success('Payment successfully processed!');
 
